738a2eefbc35c9cd72afec4a50f63ecfec85f520
HendricksK <kurvinhendricks@gmail.com>
2022-10-13 18:31:56 +0200

    init

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	fmt.Println("Starting front end service on port 80")
+	err := http.ListenAndServe(":80", nil)
+
+	if err != nil {
+		// firefox is fighting me in manjaro for port 80 usage, so adding an extra try for port 8080, should be fine in docker
+		fmt.Println("Starting front end service on port 8080")
+		finalErr := http.ListenAndServe(":8080", nil)
+		if finalErr != nil {
+			log.Panic(finalErr)
+		}
+
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	partials := []string{
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/header.partial.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
